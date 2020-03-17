48bd7d1cb3354130bcd4d9528c7dfee6e11a66d3
Leon <le.o.n@outlook.com>
2020-03-17 13:25:12 +1300

    Reading in user data into arraylists appropriately

--- PokerHands.java
+++ PokerHands.java
@@ -8,12 +8,28 @@ import java.util.*;
  */
 
 public class PokerHands{
-    //An array list of strings representing the hands the user enters
-    public static ArrayList<String> hands = new ArrayList<>();
+    //An array list of strings representing the hands the user enters, not converted to upper case
+    public static ArrayList<String> rawHands = new ArrayList<>();
+    //This array list will contain all of the strings from rawHands ArrayList, but
+    //converted to upper case
+    public static ArrayList<String> convertedHands = new ArrayList<>();
     //I'll probably redo how valid chars are held, but for now it's fine.
     //Also the order of this array represents the value of each card.
     public static Character[] valids = {'2', '3', '4', '5', '6', '7', '8', '9', 'J',
 			    'Q', 'K', 'A'};
     //An arraylist made out of valids array. useful for indexof() and get()
     public static ArrayList<Character> validOrder = new ArrayList<>(Arrays.asList(valids));
+
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	/*While user is entering data, read in data into rawHands
+	  ArrayList, and add a toUpperCase() version of the string to the
+	  convertedHands ArrayList
+	 */
+	while(sc.hasNextLine()){
+	    String in = sc.nextLine();
+	    rawHands.add(in);
+	    convertedHands.add(in.toUpperCase());
+	}
+    }
 }
