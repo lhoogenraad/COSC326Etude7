fc3cd7993976b5cb66a8fa7b9ca636705fd85072
Leon <le.o.n@outlook.com>
2020-03-17 15:07:07 +1300

    Just finishing off validateHand() method, then need to do testing, suspiciously easy so far...

--- PokerHands.java
+++ PokerHands.java
@@ -32,4 +32,77 @@ public class PokerHands{
 	    convertedHands.add(in.toUpperCase());
 	}
     }
+
+    /**
+     * This method validates a given poker hand.
+     * It achieves this by first checking that the length of the given string 
+     * is == to 14, as this is the only length a valid poker hand can be.
+     *
+     * Then it checks that only one seperator is used, and then checks that only 4 seperators
+     * have been used by using String.split(sep). 
+     * I always feel hacky and yuck using String.split() to check how many times a sequence appears
+     * in a string, but if we're splitting by a one char seperator then it should work every time 8).
+     *
+     * It then calls validate
+     */
+    private static boolean validateHand(String hand){
+	/*A valid hand can only be 14 chars long.
+	 10 chars for cards & suit types, and 4 for seperators.*/
+	if(hand.length() != 14){
+	    return false;
+	}
+	int seperators = 0;
+	String sep = "";
+	if(hand.contains("/")){
+	    seperators++;
+	    sep = "/";
+	}
+	if(hand.contains("-")){
+	    seperators++;
+	    sep = "-";
+	}
+	if(hand.contains(" ")){
+	    seperators++;
+	    sep = " ";
+	}
+	/*Only one seperator should be used, so if seperators is not 1,
+	  the hand must be invalid.*/
+	if(seperators != 1){
+	    return false;
+	}
+	/*Here we are sort of checking that only 4 seperators were used, in a sort of hacky way.*/
+	String[] handSplit = hand.split(sep);
+	if(handSplit.length != 5){
+	    return false;
+	}
+	/*Could put a try{}catch(ArrayIndexOutOfBounds) exception here in case
+	  Things go very wrong, but for now i think it's unnecessary*/
+	
+	int[] cardValues = new int[5];
+	for(String s)
+    }
+
+
+    
+
+    /**
+     * This method iterates through a given string, checking that each char in
+     * the string matches a char in the validChar array
+     */
+    private static boolean checkValidChars(String s){
+	char[] sCharArray = s.toCharArray();
+	for(Character c: sCharArray){
+	    boolean match = false;
+	    for(Character validChar : validOrder){
+		if(c == validChar){
+		    match = true;
+		    break;
+		}
+	    }
+	    if(!match){
+		return false;
+	    }
+	}
+	return true;
+    }
 }
